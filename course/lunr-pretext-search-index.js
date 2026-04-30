1e414c45c059abd8700ba144a873889df598108a
Kohl James <kjames13@svsu.edu>
2026-04-30 18:35:58 -0400

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,95 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus   Welcome to MATH-262-01 (Differential Equations)! This is the syllabus for our course. It is a document where you can find information about course policies and procedures.     Course Information     Instructor  Dr. Kohl James    Email   kjames13@svsu.edu     Meeting Room  Brown Hall Room 107    Meeting Time  Face-to-face on Mondays and Wednesdays from 5:45p-7:45p.      Office Hours  My office is located in SW-341, but I will host office hours from 3:30p-5:30p and will be held in the MPLC (see next section). I am also available for appointment if these times do not work for you. Office hours are a great place to get help on homework, ask questions about course materials, or even just have a cup of coffee and chat. They will be your single greatest resource outside of class, so please use them!    Math \\amp Physics Learning Center  The Math \\amp Physics Learning Center (MPLC) is located on the second floor of Zhanow Library. Their hours of operation are TBD . The cost of the MLPC is included in your tuition, meaning you have already paid to use it! The center has on-demand tutors and free refreshments, making it an ideal place to sit to work on your assignments in this class. You can also make a a free one-on-one appointment with a tutor by calling them at (989) 964-4648 or emailing them at mathtutor@svsu.edu .    Communication about Class  If you need to get a hold of me outside of our face-to-face sessions, I am most easily reached by email at kjames13@svsu.edu. The best times to reach me are between 10:00 am and 4:00 pm on Monday-Thursday, but I respond to emails within 24 hours. I am typically out of the office over the weekends (Saturdays and Sundays) and on holidays. During these times, I will still reply to your email within 48 hours.  I will also use email as a primary method of communicating with you. I will send reminders and announcements to the class over email. When emailing me (or addressing me in person), please feel free to address me as Kohl. If it is intimidating to call me by my first name, you may call me Mr. or Dr. James. If you are emailing me about problems or technical difficulties, it is helpful for me if you can include a screenshot\/picture of the issue. I kindly request that all email correspondence remain professional (e.g. appropriate language and grammar). Finally, it will be helpful for me if you could sign your emails with your preferred name.     What do we do in MATH-262?  The course catalog describes this course simply lists this class as covering “Exact solutions of common types of first-order ordinary differential equations, linear equations of higher order, power series solutions, Laplace transforms, linear systems including matrix methods, graphical and numerical techniques and applications of differential equations.”  I believe this description misses the bigger picture from Differential Equations. These types of equations model countless real-world phenomena, as they can describe characteristics of real systems that change throughout time and space. This course is the study of modelling such phenomena.    Student Learning Outcomes  Students that pass this course will be able to...   Classify differential equations according to their order and homogeneity.  Determine and apply appropriate solutions to first-order differential equations.  Determine and apply appropriate solution methods for different classes of higher-order differential equations.  Determine and apply appropriate solution methods for systems of differential equations.  Build and deconstruct mathematical models for real-world phenomena that are modeled by differential equations.  Approximate the solution to ordinary differential equations using simple numerical methods.         Assessments and Grades     "
+},
+{
+  "id": "act-1",
+  "level": "1",
+  "url": "act-1.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Activity 1",
+  "body": " Activity 1     Instructions: Write solutions to each of the following problems in the space provided. You are encouraged to work in groups, but also encouraged to use your own words to express your ideas.       Go to the Wikipedia entry entitled \"List of Named Differential Equations\". For each of the following categories, chose and write down a differential equation that stands out to you and one listed application for it. Identify the equations order, then state whether it is ordinary or partial. If it is ordinary, determine whether it is linear.     Math      Physics      Engineering      Chemistry      Biology and medicine      Economics and finance         For each of the following, write an initial value problem to model the scenario.      Population growth: The culture was seeded with 500 bacteria, whose population growth rate at time grew proportionally to its population.       Harmonic Oscillator: A mass kg is attached to a spring and hung from the ceiling. The spring is stretched by meters from its equilibrium position and is let go, setting the system in (bouncy, springy) motion. Let denote the spring's position at time .          Solar Collector: A solar collector is shaped like the solid generated by revolving the curve about the -axis. Its purpose is to reflect all light light rays parallel to its axis of symmetry to the same point (the origin). The Law of reflection tells us that the angle of incedence and reflection are equal. Write a differential equation that describes the shape of .     Try showing the the angle the reflected ray makes with respect to the -axis is exactly twice the angle the tangent line to makes with respect to the axis.     "
+},
+{
+  "id": "act-1-3-1",
+  "level": "2",
+  "url": "act-1.html#act-1-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Go to the Wikipedia entry entitled \"List of Named Differential Equations\". For each of the following categories, chose and write down a differential equation that stands out to you and one listed application for it. Identify the equations order, then state whether it is ordinary or partial. If it is ordinary, determine whether it is linear.     Math      Physics      Engineering      Chemistry      Biology and medicine      Economics and finance    "
+},
+{
+  "id": "act-1-4-1",
+  "level": "2",
+  "url": "act-1.html#act-1-4-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  For each of the following, write an initial value problem to model the scenario.      Population growth: The culture was seeded with 500 bacteria, whose population growth rate at time grew proportionally to its population.       Harmonic Oscillator: A mass kg is attached to a spring and hung from the ceiling. The spring is stretched by meters from its equilibrium position and is let go, setting the system in (bouncy, springy) motion. Let denote the spring's position at time .    "
+},
+{
+  "id": "act-1-5-1",
+  "level": "2",
+  "url": "act-1.html#act-1-5-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "   Solar Collector: A solar collector is shaped like the solid generated by revolving the curve about the -axis. Its purpose is to reflect all light light rays parallel to its axis of symmetry to the same point (the origin). The Law of reflection tells us that the angle of incedence and reflection are equal. Write a differential equation that describes the shape of .     Try showing the the angle the reflected ray makes with respect to the -axis is exactly twice the angle the tangent line to makes with respect to the axis.   "
+},
+{
+  "id": "hw1",
+  "level": "1",
+  "url": "hw1.html",
+  "type": "Section",
+  "number": "",
+  "title": "Homework 1",
+  "body": " Homework 1    Due Date: This assignment is due at the beginning of class on Wednesday, July 1.    Practice Problems  It is strongly recommended that you complete the below practice problems from the course textbook before beginning the assignment. These solutions do not need to be submitted and will not be graded.   Exercises 1.1 [pg.10] #1-4, 11-12, 21-22, and 25.    Exercises 1.2 [pg.17] #1-2, 7-8, 11-12, 29, and 35-38.    Exercises 1.3 [pg.27] #1-2, 5, 7, 9-10, 13, 15.       Assigned Problems    Instructions: Write solutions to the following problems in the space provided. If you need additional space, you may attach scratch work to the back of this sheet. Please be sure any additional work is clearly labeled with the question it corresponds to.      Provide an example of a second-order, linear differential equation. Justify your answer.      Find an interval on which the function is a solution to the differential equation         A tank is holding 1000L of pure water. A brine solution (0.1 kg\/L) is poured into the tank at a constant rate of 6L\/min. The solution in the tank is always kept well-stirred and is drained out of the tank at 6 L\/min.     Draw a picture of the scenario.      Identify and label the dependent and independent variabels in the scenario.      Write an initial value problem that models the amount of salt in the tank over time.      Solve the initial value problem you created in the above problem.       "
+},
+{
+  "id": "hw1-assigned-3-1",
+  "level": "2",
+  "url": "hw1.html#hw1-assigned-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Provide an example of a second-order, linear differential equation. Justify your answer.   "
+},
+{
+  "id": "hw1-assigned-3-2",
+  "level": "2",
+  "url": "hw1.html#hw1-assigned-3-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Find an interval on which the function is a solution to the differential equation    "
+},
+{
+  "id": "hw1-assigned-4-1",
+  "level": "2",
+  "url": "hw1.html#hw1-assigned-4-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  A tank is holding 1000L of pure water. A brine solution (0.1 kg\/L) is poured into the tank at a constant rate of 6L\/min. The solution in the tank is always kept well-stirred and is drained out of the tank at 6 L\/min.     Draw a picture of the scenario.      Identify and label the dependent and independent variabels in the scenario.      Write an initial value problem that models the amount of salt in the tank over time.      Solve the initial value problem you created in the above problem.    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
